c4df55a673011cd231a10dc24ed65daae5185a8a
ry2x <45420571+ry2x@users.noreply.github.com>
2025-02-23 08:09:36 +0900

    Update Ping

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,9 +1,18 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { Colors, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import ApplicationCommand from '../templates/ApplicationCommand.js';
 
 export default new ApplicationCommand({
-  data: new SlashCommandBuilder().setName('ping').setDescription('Replies pong!'),
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('現在のサーバー間(Discord⇔BOT)の遅延を表示します。'),
   async execute(interaction): Promise<void> {
-    await interaction.reply('Pong!');
+    const embed = (description: string): EmbedBuilder => {
+      return new EmbedBuilder()
+        .setColor(Colors.Blue)
+        .setTitle('Ping')
+        .setDescription(`ping値は${description}msです`);
+    };
+
+    await interaction.reply({ embeds: [embed(client.ws.ping.toString())], ephemeral: true });
   },
 });
