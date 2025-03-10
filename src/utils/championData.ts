43d01329a5c89a4217bb2779950201fd5ae698c0
ry2x <yuli121220.big@gmail.com>
2025-03-10 14:13:36 +0000

    improve champion list 2

--- src/utils/championData.ts
+++ src/utils/championData.ts
@@ -3,13 +3,38 @@ import config from '../config.json' with { type: 'json' };
 import logger from '../logger.js';
 import type { Champion, Champions } from '../types/interface.js';
 
-export const lanes = {
-  all: 'all',
-  top: 'top',
-  jg: 'jg',
-  mid: 'mid',
-  ad: 'ad',
-  sup: 'sup',
+export type LaneKey = 'all' | 'top' | 'jg' | 'mid' | 'ad' | 'sup';
+
+export type Lane = {
+  name: string;
+  value: LaneKey;
+};
+
+export const lanes:Record<LaneKey, Lane> = {
+  all: {
+    name: 'All (全レーン)',
+    value: 'all',
+  },
+  top: {
+    name: 'Top (トップ)',
+    value: 'top',
+  },
+  jg: {
+    name: 'Jungle (ジャングル)',
+    value: 'jg',
+  },
+  mid: {
+    name: 'Mid (ミッド)',
+    value: 'mid',
+  },
+  ad: {
+    name: 'ADC (ボット)',
+    value: 'ad',
+  },
+  sup: {
+    name: 'Support (サポート)',
+    value: 'sup',
+  },
 };
 
 let champions: Champions = {};
@@ -39,17 +64,17 @@ export function getChampionNames() {
 export function getChampionsByLane(lane: string) {
   return Object.values(champions).filter((champ: Champion) => {
     switch (lane) {
-      case lanes.all:
+      case lanes.all.value:
         return Object.values(champions);
-      case lanes.top:
+      case lanes.top.value:
         return champ.is_top;
-      case lanes.jg:
+      case lanes.jg.value:
         return champ.is_jg;
-      case lanes.mid:
+      case lanes.mid.value:
         return champ.is_mid;
-      case lanes.ad:
+      case lanes.ad.value:
         return champ.is_ad;
-      case lanes.sup:
+      case lanes.sup.value:
         return champ.is_sup;
       default:
         return false;
@@ -59,17 +84,17 @@ export function getChampionsByLane(lane: string) {
 
 export function getLaneEmoji(lane: string) {
   switch (lane) {
-    case lanes.all:
+    case lanes.all.value:
       return '<:Lane_All:1343842075464175616>';
-    case lanes.top:
+    case lanes.top.value:
       return '<:Lane_Top:1343276732194750485>';
-    case lanes.jg:
+    case lanes.jg.value:
       return '<:Lane_Jungle:1343276691853934647>';
-    case lanes.mid:
+    case lanes.mid.value:
       return '<:Lane_Mid:1343276706143932447>';
-    case lanes.ad:
+    case lanes.ad.value:
       return '<:Lane_Bot:1343276674044792974>';
-    case lanes.sup:
+    case lanes.sup.value:
       return '<:Lane_Support:1343276719049543803>';
     default:
       return '';
