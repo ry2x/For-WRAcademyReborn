8d33b642b6820bb3a5f8b50c5ed5eb5ebadec333
ry2x <45420571+ry2x@users.noreply.github.com>
2025-03-16 15:32:26 +0000

    Update schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const users = pgTable(
   'users',
@@ -9,21 +9,6 @@ export const users = pgTable(
     lastXpAt: timestamp('last_xp_at', { withTimezone: true }).defaultNow().notNull(),
     nextLevelXp: integer('next_level_xp').notNull().default(100),
     joinedAT: timestamp('joined_at', { withTimezone: true }).defaultNow().notNull(),
-  },
-  (table) => ({
-    schema: 'bot',
-  }),
-);
-
-export const userLevels = pgTable(
-  'user_levels',
-  {
-    id: serial('id').primaryKey(),
-    userId: text('user_id')
-      .notNull()
-      .references(() => users.id, { onDelete: 'cascade' }),
-    xpGained: integer('xp_gained').notNull(),
-    timestamp: timestamp('timestamp', { withTimezone: true }).defaultNow().notNull(),
   },
   (table) => ({
     schema: 'bot',
