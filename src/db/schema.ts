2e0eb50c61234ea04008eca880ff5a471fe9f277
ry2x <45420571+ry2x@users.noreply.github.com>
2025-03-18 07:38:14 +0900

    Switch DB from NEON

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,16 +1,11 @@
-import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { integer, pgSchema, text, timestamp } from 'drizzle-orm/pg-core';
 
-export const users = pgTable(
-  'users',
-  {
-    id: text('id').primaryKey(),
-    level: integer('level').notNull().default(1),
-    xp: integer('xp').notNull().default(0),
-    lastXpAt: timestamp('last_xp_at', { withTimezone: true }).defaultNow().notNull(),
-    nextLevelXp: integer('next_level_xp').notNull().default(100),
-    joinedAT: timestamp('joined_at', { withTimezone: true }).defaultNow().notNull(),
-  },
-  (table) => ({
-    schema: 'bot',
-  }),
-);
+const bot = pgSchema('bot');
+export const users = bot.table('users', {
+  id: text('id').primaryKey(),
+  level: integer('level').notNull().default(1),
+  xp: integer('xp').notNull().default(0),
+  lastXpAt: timestamp('last_xp_at', { withTimezone: true }).defaultNow().notNull(),
+  nextLevelXp: integer('next_level_xp').notNull().default(100),
+  joinedAT: timestamp('joined_at', { withTimezone: true }).defaultNow().notNull(),
+});
