4289f5d0d5afd3d19a9d4b3b8b5799f1f2ee9117
Ry2X <45420571+ry2x@users.noreply.github.com>
2025-04-02 16:31:18 +0000

    Refactoring for new expression

--- src/subCommands/champion/team.ts
+++ src/subCommands/champion/team.ts
@@ -1,41 +1,42 @@
+import { getChampionsByLane, getLaneEmoji, lanes } from '@/data/championData.js';
+import { interactionErrorEmbed } from '@/embeds/errorEmbed.js';
 import SubCommand from '@/templates/SubCommand.js';
 import { Colors, EmbedBuilder, MessageFlags, type ChatInputCommandInteraction } from 'discord.js';
-import { getChampionsByLane, getLaneEmoji } from '@/data/championData.js';
-import { interactionErrorEmbed } from '@/embeds/errorEmbed.js';
 
 export default new SubCommand({
   async execute(interaction: ChatInputCommandInteraction): Promise<void> {
     await interaction.deferReply();
 
     const wrOnly = interaction.options.getBoolean('wr_only') ?? true;
-    const lanes = ['top', 'jg', 'mid', 'ad', 'sup'];
     const team: { [key: string]: string[] } = {};
     const selectedChamps = new Set<string>();
 
-    for (const lane of lanes) {
-      let champions = getChampionsByLane(lane);
-      if (wrOnly) {
-        champions = champions.filter((c) => c.is_wr);
-      }
+    await Promise.all(
+      Object.entries(lanes).map(async ([key, v]) => {
+        let champions = getChampionsByLane(v.value);
+        if (wrOnly) {
+          champions = champions.filter((c) => c.is_wr);
+        }
 
-      champions = champions.filter((c) => !selectedChamps.has(c.id));
+        champions = champions.filter((c) => !selectedChamps.has(c.id));
 
-      if (champions.length < 2) {
-        await interaction.deleteReply();
-        await interaction.followUp({
-          embeds: [
-            interactionErrorEmbed(`❌${lane.toUpperCase()} にチャンピオンが不足しています。`),
-          ],
-          flags: MessageFlags.Ephemeral,
-        });
-        return;
-      }
+        if (champions.length < 2) {
+          await interaction.deleteReply();
+          await interaction.followUp({
+            embeds: [
+              interactionErrorEmbed(`❌${v.value.toUpperCase()} にチャンピオンが不足しています。`),
+            ],
+            flags: MessageFlags.Ephemeral,
+          });
+          return;
+        }
 
-      const selected = champions.sort(() => 0.5 - Math.random()).slice(0, 2);
-      team[lane] = selected.map((c) => c.name);
+        const selected = champions.sort(() => 0.5 - Math.random()).slice(0, 2);
+        team[key] = selected.map((c) => c.name);
 
-      selected.forEach((c) => selectedChamps.add(c.id));
-    }
+        selected.forEach((c) => selectedChamps.add(c.id));
+      }),
+    );
 
     const embed = new EmbedBuilder()
       .setTitle(
