eb6d3a6bd4faec1ff84d2d217aefdc65d21a26ba
Ry2X <45420571+ry2x@users.noreply.github.com>
2025-04-02 16:30:15 +0000

    update types

--- src/types/winRate.ts
+++ src/types/winRate.ts
@@ -2,7 +2,7 @@
 export type HeroStats = {
   id: number;
   position: string;
-  hero_id: string;
+  hero_id: number;
   strength: string;
   weight: string;
   appear_rate: string;
@@ -22,12 +22,10 @@ export type HeroStats = {
 };
 
 // 1:mid 2:top 3:adc 4:sup 5:jg
-export type lane = '1' | '2' | '3' | '4' | '5';
+export type lane = '1' | '2' | '3' | '4' | '5' | '0';
 
 // lane stats from CN winRateAPI
-export type PositionStats = {
-  [K in lane]: HeroStats[];
-};
+export type PositionStats = Partial<Record<lane, HeroStats[]>>;
 
 // 0:ALL 1:Dia+ 2:Mas+ 3:Ch+ 4:super server
 export type rankRange = '0' | '1' | '2' | '3' | '4';
