8525e790f3e33e8c993db20f016cdfa9749f8048
Ry2X <45420571+ry2x@users.noreply.github.com>
2025-05-01 22:46:32 +0900

    feat: add 'champion' namespace

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -13,7 +13,7 @@ export async function initI18n(): Promise<i18n> {
   await i18next.use(Backend).init({
     supportedLngs: ['en_US'],
     fallbackLng: process.env.DEFAULT_LOCALE ?? 'en_US',
-    ns: ['common'],
+    ns: ['common', 'champion'],
     defaultNS: 'common',
     interpolation: {
       escapeValue: false,
